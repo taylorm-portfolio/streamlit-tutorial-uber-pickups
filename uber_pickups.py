e3f9fd1d6ae5c1ccc907fd798794c94880eb981a
taylorm-portfolio <153951705+taylorm-portfolio@users.noreply.github.com>
2024-09-30 14:06:37 -0500

    Update uber_pickups.py
    Removed slider from histogram, moved raw data to bottom

--- uber_pickups.py
+++ uber_pickups.py
@@ -17,6 +17,7 @@ def load_data(nrows):
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
+
 # Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
 # Load 10,000 rows of data into the dataframe.
@@ -24,25 +25,26 @@ data = load_data(10000)
 # Notify the reader that the data was successfully loaded.
 data_load_state.text("Done! (using st.cache_data)")
 
-# set up slider for filtering by hour
+# Add histogram
+
+st.subheader('Number of pickups by hour')
+
+hist_values = np.histogram(
+    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+
+st.bar_chart(hist_values)
+
+# Add slider to filter
 hour_to_filter = st.slider('hour', 0, 23, 17)
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+# Add Map chart
+st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+
+st.map(filtered_data)
 
 # Inspect the raw data
 
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(filtered_data)
-
-# Add histogram
-
-st.subheader('Number of pickups by hour')
-
-hist_values = np.histogram(
-    filtered_data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-
-st.bar_chart(hist_values)
-
-st.subheader(f'Map of all pickups at {hour_to_filter}:00')
-
-st.map(filtered_data)
