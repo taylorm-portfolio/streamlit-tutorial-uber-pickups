58ef0916c19a99d13d268e3bf98ed4101451c9bd
taylorm-portfolio <153951705+taylorm-portfolio@users.noreply.github.com>
2024-09-30 14:03:53 -0500

    Update uber_pickups.py
    Applied slider to all objects

--- uber_pickups.py
+++ uber_pickups.py
@@ -17,7 +17,6 @@ def load_data(nrows):
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
-
 # Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
 # Load 10,000 rows of data into the dataframe.
@@ -25,23 +24,25 @@ data = load_data(10000)
 # Notify the reader that the data was successfully loaded.
 data_load_state.text("Done! (using st.cache_data)")
 
+# set up slider for filtering by hour
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
 # Inspect the raw data
 
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
-    st.write(data)
+    st.write(filtered_data)
 
 # Add histogram
 
 st.subheader('Number of pickups by hour')
 
 hist_values = np.histogram(
-    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+    filtered_data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 
 st.bar_chart(hist_values)
 
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 st.subheader(f'Map of all pickups at {hour_to_filter}:00')
 
 st.map(filtered_data)
